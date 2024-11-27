f43bfb1d5e8df55448fa82b1a18ec0056d934618
huyle <huyle210504@gmail.com>
2024-11-27 12:32:16 +0700

    m

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        keys = {
+            { "<C-\\>", "<cmd>:ToggleTerm<CR>", {} }
+        },
+        opts = {
+            direction = "float",
+        },
+    },
+}
