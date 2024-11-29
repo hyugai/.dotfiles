0c0da0b6d7b00ed91f03af8b1cf4a5504735082b
noob <huyle210504@gmail.com>
2024-11-29 21:18:41 +0700

    m

--- install_lastestNeoVim.sh
+++ install_lastestNeoVim.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-cd ~
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-rm -rf /opt/nvim
-tar -C /opt -xzf nvim-linux64.tar.gz
+rm -rf /opt/nvim /opt/nvim-linux64
+tar -C /opt -xzf nvim-linux64.tar.gz 
+rm -rf nvim-linux64.tar.gz 
 
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
-source ~/.bashrc
+nvim_path=$(grep 'export PATH="$PATH:/opt/nvim-linux64/bin"' ~/.bashrc | wc -l)
+if [ nvim_path = 1 ]; then
+	echo 'path of nvim has already existed!'
+else
+	echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+	source ~/.bashrc
+fi
