a71aa1c8b93ce8513114470eb0c57b1fcd99e5a4
huyle <huyle210504@gmail.com>
2024-12-02 10:00:20 +0700

    m

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,53 +1,54 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({})
-		end,
-	},
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({})
+        end,
+    },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ruff",
-					"pyright",
-				},
-			})
-		end,
-	},
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "ruff",
+                    "pyright",
+                    "rust_analyzer",
+                },
+            })
+        end,
+    },
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-				settings = {
-					pyright = {
-						disableOrganizeImports = true,
-					},
-					python = {
-						analysis = {
-							ignore = { "*" },
-						},
-					},
-				},
-			})
-			lspconfig.ruff.setup({
-				capabilities = capabilities,
-			})
-		end,
-		keys = {
-			{ "K", vim.lsp.buf.hover, {} },
-			{ "<leader>ca", vim.lsp.buf.code_action, {} },
-		},
-	},
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+                settings = {
+                    pyright = {
+                        disableOrganizeImports = true,
+                    },
+                    python = {
+                        analysis = {
+                            ignore = { "*" },
+                        },
+                    },
+                },
+            })
+            lspconfig.ruff.setup({
+                capabilities = capabilities,
+            })
+        end,
+        keys = {
+            { "K",          vim.lsp.buf.hover,       {} },
+            { "<leader>ca", vim.lsp.buf.code_action, {} },
+        },
+    },
 }
