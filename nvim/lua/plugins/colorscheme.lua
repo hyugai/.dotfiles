25cfec034896177a5c06e2fecbbad1df86a97db3
hyugai <huyle210504@gmail.com>
2025-05-25 16:56:58 +0700

    m

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,19 @@ return {
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		require("github-theme").setup({})
+		require("github-theme").setup({
+			options = {
+				styles = {
+					comments = "italic",
+					functions = "bold",
+					keywords = "italic,bold",
+					conditionals = "italic",
+					constants = "bold",
+					types = "italic",
+				},
+				transparent = true,
+			},
+		})
 
 		vim.cmd("colorscheme github_dark_colorblind")
 	end,
