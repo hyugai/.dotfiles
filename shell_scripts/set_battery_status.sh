11a83431aefb2c066fffd772e9cb532629fa8da7
cookies <huyle210504@gmail.com>
2025-10-01 06:07:39 +0700

    got a bug in tmux bar

--- shell_scripts/set_battery_status.sh
+++ shell_scripts/set_battery_status.sh
@@ -1,18 +1,58 @@
 #!/bin/bash
+# → nf-fa-battery_empty
+#
+# → nf-fa-battery_quarter
+#
+# → nf-fa-battery_half
+#
+# → nf-fa-battery_three_quarters
+#
+# → nf-fa-battery_full
+# (nf-fa-plug) → plug
+#
+# (nf-fa-bolt) → bolt
 
 #debug this function
 get_battery_info() {
-    local BAT_count=0
-    local total_elementary_capacities=0
-    local capacity
+    local status
 
-    for i in /sys/class/power_supply/BAT*; do
-        BAT_count=$((BAT_count + 1))
-        total_elementary_capacities=$((total_elementary_capacities + $(cat "$i"/capacity)))
-    done
+    _sum_capacity() {
+        local capacity=0
+        local BAT_count=0
+        echo "hello"
 
-    capacity=$((total_elementary_capacities / BAT_count))
+        while [[ $# -gt 0 ]]; do
+            capacity=$((capacity + $(cat "$1")))
+            BAT_count=$((BAT_count + 1))
 
-    echo $capacity
+            shift
+        done
+
+        echo $((capacity / BAT_count))
+    }
+
+    say_hi() {
+        echo hello
+    }
+
+    main() {
+        case "$1" in
+        Charging)
+            echo ""
+            ;;
+        Discharging)
+            local avg_capacity
+            # bug's here
+            find -L /sys/class/power_supply/BAT* -maxdepth 1 -type f -name capacity -exec bash -c say_hi '{}' +
+            #echo "$avg_capacity"
+            ;;
+        Full)
+            echo '  100%'
+            ;;
+        esac
+    }
+
+    status=$(find -L /sys/class/power_supply/BAT* -maxdepth 1 -type f -name status -exec cat '{}' \;)
+    main "$status"
 }
 get_battery_info
